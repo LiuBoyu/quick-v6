e727946f80ebc5b121181e43e8a0197bea4c51b2
LiuBoyu <boyu.liu@gmail.com>
2016-12-20 14:05:56 +0800

    修复Android项目标准模板

--- proj/proj.android/SPS-V6/build.sh
+++ proj/proj.android/SPS-V6/build.sh
@@ -13,6 +13,9 @@ export QUICK_V3_ROOT="../../../../quick-v3"
 COMP="$QUICK_V3_ROOT/quick/bin/compile_scripts.sh"
 OPTS="-e xxtea_zip -ek $2 -es XT"
 
+rm -rf src/release/assets/zip
+mkdir  src/release/assets/zip
+
 $COMP $OPTS -jit -r -i ../../../src/framework  -p framework  -o src/release/assets/zip/framework.zip
 $COMP $OPTS -jit -r -i ../../../src/$1         -p $1         -o src/release/assets/zip/$1.zip
 
