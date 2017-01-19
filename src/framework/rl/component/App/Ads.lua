b020451f26c29c9c5d9e19fa7057b8d8ef44de62
LiuBoyu <boyu.liu@gmail.com>
2017-01-20 02:09:50 +0800

    添加 App.Ads - playAd 方法

--- src/framework/rl/component/App/Ads.lua
+++ src/framework/rl/component/App/Ads.lua
@@ -21,7 +21,45 @@ return function(object, args)
 -- 对象方法·行为逻辑
 ----------------------------------------
 
-    -- nothing
+    function object:playAd(callback)
+        local isOK
+
+        local callback = function(ret)
+
+            if ret.error then
+                    self:dispatchEvent("ADS_PLAY_FAILED"   , ret)
+            else
+                if ret.isCompleted then
+                    self:dispatchEvent("ADS_PLAY_COMPLETED", ret)
+                else
+                    self:dispatchEvent("ADS_PLAY_CANCELLED", ret)
+                end
+            end
+
+            if callback then
+                return callback(ret)
+            end
+        end
+
+        self:dispatchEvent("ADS_TRY_TO_PLAY")
+
+        if not isOK and args.vungle then
+            if self:isAdPlayableByVungleSDK() then
+                self:playAdByVungleSDK(function(ret)
+                    return callback({ isCompleted = ret.wasSuccessfulView, error = ret.reason })
+                end)
+                isOK = true
+            end
+        end
+
+        if not isOK then
+            return callback({ error = "unavailable" })
+        end
+    end
+
+    function object:isAdPlayable()
+        if self:isAdPlayableByVungleSDK() then return true end
+    end
 
     return component
 end
