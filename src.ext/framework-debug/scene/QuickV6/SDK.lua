1780bf83687b22a56b2e02ea9def5025be478b8c
LiuBoyu <boyu.liu@gmail.com>
2017-01-20 17:52:49 +0800

    重构 SDK 样例

--- src.ext/framework-debug/scene/QuickV6/SDK.lua
+++ src.ext/framework-debug/scene/QuickV6/SDK.lua
@@ -3,14 +3,18 @@ local M = class("UnitTest", function()
     return display.newScene()
 end)
 
-function M:ctor(args)
-    UI:DebugScene(self, args)
+function M:ctor()
+    UI:DebugScene(self)
+
+    ----------------
+    -- App
+    ----------------
 
     self:MENU("App")
 
-    self:TEST("info", function()
-        print(tostring(G.App:getDeviceInfo()))
+    self:TEST("appInfo", function()
         print(tostring(G.App:getAppInfo()))
+        print(tostring(G.App:getDevInfo()))
         print(tostring(G.App:getUdid()))
         print(tostring(G.App:getUuid()))
     end)
@@ -27,7 +31,22 @@ function M:ctor(args)
         G.App:openUrl("http://www.baidu.com/")
     end)
 
+    ----------------
+    -- IAP
+    ----------------
+
     self:MENU("IAP", { newline = false })
+
+    self:TEST("init", function()
+        G.App:initByIAPSDK(function(isOK, info)
+            print(tostring(isOK))
+            print(tostring(info))
+        end)
+    end)
+
+    self:TEST("purchase", function()
+        G.App:purchaseByIAPSDK( G.Config.IAP[1].iap )
+    end)
 
     self:TEST("onPurchaseFinished", function()
         G.App:setOnPurchaseFinishedByIAPSDK(function(ts)
@@ -36,99 +55,11 @@ function M:ctor(args)
         end)
     end)
 
-    self:TEST("init", function()
-        rl.G.IapConsumableProductIds = G.Product:getAllIAPs()
-        G.App:initByIAPSDK(function(isOK, info)
-            print(tostring(isOK))
-            print(tostring(info))
-        end)
-    end)
+    ----------------
+    -- Vungle
+    ----------------
 
-    self:TEST("purchase", function()
-        G.App:purchaseByIAPSDK( G.Product:getAllIAPs()[1] )
-    end)
-
-    self:MENU("FacebookSDK")
-
-    self:TEST("onAccessTokenChanged", function()
-        G.App:setOnCurrentAccessTokenChangedByFacebookSDK(function(old, current)
-            print("onCurrentAccessTokenChanged")
-            print(tostring(old    ))
-            print(tostring(current))
-        end)
-    end)
-
-    self:TEST("logIn", function()
-        G.App:logInByFacebookSDK(function()
-            print("logIn")
-        end)
-    end)
-
-    self:TEST("logOut", function()
-        G.App:logOutByFacebookSDK()
-        print("logOut")
-    end)
-
-    self:TEST("getToken", function()
-        local token = G.App:getCurrentAccessTokenByFacebookSDK()
-        print(tostring(token))
-    end)
-
-    self:TEST("request", function()
-        G.App:requestByFacebookSDK({
-            recipients = {},
-            message    = "Hello",
-            title      = "World",
-            callback   = function(res)
-                print(tostring(res))
-            end,
-        })
-    end)
-
-    self:TEST("Me", function()
-        G.App:GETbyFacebookSDK({
-            graphPath  = "/me",
-            parameters = {
-                fields = "id,name,first_name,last_name,email,picture.height(160).width(160)",
-            },
-            callback   = function(res)
-                print(tostring(res))
-                 G.Me:set("fb_name",res.name)
-                            G.Me:set("fb_id",res.id)
-                            G.Me:set("fb_picture_heigh",res.picture.data.height)
-                            G.Me:set("fb_picture_weight",res.picture.data.weight)
-                            G.Me:set("fb_is_silhouette",res.picture.data.is_silhouette)
-                            G.Me:set("fb_picture_weight",res.picture.data.url)
-            end,
-        })
-    end)
-
-    self:TEST("Friends", function()
-        G.App:GETbyFacebookSDK({
-            graphPath  = "/me/friends",
-            parameters = {
-                fields = "id,name,first_name,last_name,email,picture.height(160).width(160)",
-            },
-            callback   = function(res)
-                print(tostring(res))
-            end,
-        })
-    end)
-
-    self:TEST("Invitable", function()
-        G.App:GETbyFacebookSDK({
-            graphPath  = "/me/invitable_friends",
-            parameters = {
-                fields = "id,name,first_name,last_name,email,picture.height(160).width(160)",
-            },
-            callback   = function(res)
-                print(tostring(res))
-                print(#res.data)
-            end,
-        })
-    end)
-
-    self:MENU("VungleSDK")
+    self:MENU("Vungle", { newline = false })
 
     self:TEST("playAd", function()
         G.App:playAdByVungleSDK(function(ret)
@@ -140,7 +71,7 @@ function M:ctor(args)
         print("isAdPlayable: ", G.App:isAdPlayableByVungleSDK())
     end)
 
-    self:TEST("setOnAdPlayableChanged", function()
+    self:TEST("onAdPlayableChanged", function()
         G.App:setOnAdPlayableChangedByVungleSDK(function(isOK)
             print("onAdPlayableChanged:", isOK)
         end)
--- src.ext/samples-debug/config.lua
+++ src.ext/samples-debug/config.lua
@@ -7,6 +7,9 @@ G.Debug = {}
 
 G.Config.Module["@@QuickV6"]            = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "MainScene" }
 G.Config.Module["@@QuickV6.Component"]  = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "Component" }
+G.Config.Module["@@QuickV6.SDK"]        = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "SDK"       }
+G.Config.Module["@@QuickV6.UI"]         = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "UI"        }
+G.Config.Module["@@QuickV6.Facebook"]   = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "Facebook"  }
 G.Config.Module["@@QuickV6.Utils"]      = { searchPaths = { "framework-debug.scene.QuickV6" }, goto = true, gotoName = "Utils"     }
 
 G.Config.Module["@@Main"]               = { searchPaths = { PROJNS.."-debug.scene.StartUp"  }, goto = true, gotoName = "MainScene" }
