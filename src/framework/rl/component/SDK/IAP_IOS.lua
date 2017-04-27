34ae4258c6a672abae01481779b620e5cf067a1c
LiuBoyu <boyu.liu@gmail.com>
2017-04-27 15:57:24 +0800

    更新 ios Iap SDK lua

--- src/framework/rl/component/SDK/IAP_IOS.lua
+++ src/framework/rl/component/SDK/IAP_IOS.lua
@@ -15,8 +15,6 @@ return function(object, args)
 
     local args = args or {}
 
-    local onPurchaseFinished = function(ts) end
-
 ----------------------------------------
 -- 对象方法
 ----------------------------------------
@@ -24,7 +22,65 @@ return function(object, args)
     function object:setOnPurchaseFinishedByIAPSDK(callback)
         log.debug("setOnPurchaseFinished")
 
-        onPurchaseFinished = callback
+        local ok, ret = luaoc.callStaticMethod("SDKIAP", "init",  {
+                                    callback   = callback,
+                        })
+        return ret
+    end
+
+----------------------------------------
+-- 对象方法
+----------------------------------------
+
+    local isQueryInventoryDone
+    local isQueryInventoryBusy
+    local queryInventoryResult
+
+    local function queryInventory(callback)
+        log.debug("queryInventory")
+
+        if isQueryInventoryBusy then
+            return callback({ code = -1 })
+        end
+
+        if isQueryInventoryDone then
+            return callback(nil, queryInventoryResult)
+        end
+
+        isQueryInventoryBusy = true
+
+        local callback = function(products, invalidIds, e)
+            isQueryInventoryBusy = nil
+
+            if not e then
+                isQueryInventoryDone = true
+
+                luaoc.callStaticMethod("SDKIAP", "loadStore")
+
+                -- fixme
+                queryInventoryResult = { consumableProducts = products, nonConsumableProducts = nil, invalidIds = invalidIds }
+            end
+
+            return callback(e, queryInventoryResult)
+        end
+
+        local consumableProductIds    = rl.G.IapConsumableProductIds    or {}
+        local nonConsumableProductIds = rl.G.IapNonConsumableProductIds or {}
+
+        local productIds = {}
+
+        for _, v in ipairs(   consumableProductIds) do
+            productIds[#productIds+1] = v
+        end
+        for _, v in ipairs(nonConsumableProductIds) do
+            productIds[#productIds+1] = v
+        end
+
+        local ok, ret = luaoc.callStaticMethod("SDKIAP", "loadProducts",  {
+                                    productIds = productIds and json.encode(productIds) or nil,
+                                    callback   = callback,
+                        })
+        return ret
     end
 
 ----------------------------------------
@@ -35,21 +91,41 @@ return function(object, args)
         log.debug("init")
 
         local callback = callback or function(isOK, info) end
-        return callback(true, {})
+        queryInventory(function(e, res)
+            if e then return callback(false) end
+            return callback(true, res)
+        end)
     end
 
     function object:isInitDoneByIAPSDK()
-        return true
+        return isQueryInventoryDone
     end
 
 ----------------------------------------
 -- 对象方法
 ----------------------------------------
 
-    function object:purchaseByIAPSDK(id)
-        log.debug("purchase: %s", { id = id })
+    local function checkInitDone(callback)
+        if object:isInitDoneByIAPSDK() then
+            return callback(true)
+        end
 
-        onPurchaseFinished({ state = "Purchased", productId = id, receipt = "" })
+        object:initByIAPSDK(function(isOK, info)
+            return callback(isOK)
+        end)
+    end
+
+    function object:purchaseByIAPSDK(id)
+        checkInitDone(function(isOK)
+            log.debug("purchase: %s", { isOK = isOK, id = id })
+
+            if isOK then
+                local ok, ret = luaoc.callStaticMethod("SDKIAP", "purchase",  {
+                                    productId = id,
+                                })
+                return ret
+            end
+        end)
     end
 
     return component
