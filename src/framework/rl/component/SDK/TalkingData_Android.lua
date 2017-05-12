017ed679f4b9d206bc5356eb9c5d43b1840ddf00
LiuBoyu <boyu.liu@gmail.com>
2017-05-12 14:24:19 +0800

    添加 TalkingData SDK (android)

--- src/framework/rl/component/SDK/TalkingData_Android.lua
+++ src/framework/rl/component/SDK/TalkingData_Android.lua
@@ -20,44 +20,85 @@ return function(object, args)
 -- 对象方法
 ----------------------------------------
 
-    function object:onProfileSignInByTalkingDataSDK(accountId)
-        log.debug("onProfileSignIn: %s", accountId)
-
-        local accountId = accountId or "Unknown"
-
-        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onProfileSignIn", {
-                            accountId
-                        }, "(Ljava/lang/String;)V")
-        return ret
-    end
-
-    function object:onProfileSignOffByTalkingDataSDK()
-        log.debug("onProfileSignOff")
-
-        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onProfileSignOff", {
-                        }, "()V")
-        return ret
-    end
-
     function object:onEventByTalkingDataSDK(e)
         log.debug("onEvent: %s", e)
 
-        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onEvent", {
-                            json.encode({ id = e.id, kv = e.kv, du = e.du })
-                        }, "(Ljava/lang/String;)V")
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onEvent", { json.encode({
+
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
+    end
+
+    function object:onLevelByTalkingDataSDK(e)
+        log.debug("onLevel: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onLevel", { json.encode({
+                                     cmd = e.cmd,
+                                   level = e.level,
+                                   cause = e.cause,
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
+    end
+
+    function object:onUserByTalkingDataSDK(e)
+        log.debug("onUser: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onUser", { json.encode({
+                                     cmd = e.cmd,
+                                playerID = e.playerID,
+                                provider = e.provider,
+                                   level = e.level,
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
+    end
+
+    function object:onPayByTalkingDataSDK(e)
+        log.debug("onPay: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onPay", { json.encode({
+                                     cmd = e.cmd,
+                                    cash = e.cash,
+                                    coin = e.coin,
+                                    item = e.item,
+                                  amount = e.amount,
+                                   price = e.price,
+                                  source = e.source,
+                                 orderId = e.orderId,
+                                   iapId = e.iapId,
+                        }) }, "(Ljava/lang/String;)V")
         return ret
     end
 
     function object:onBuyByTalkingDataSDK(e)
         log.debug("onBuy: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onBuy", { json.encode({
+                                    item = e.item,
+                                  amount = e.amount,
+                                   price = e.price,
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
     end
 
     function object:onUseByTalkingDataSDK(e)
         log.debug("onUse: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onUse", { json.encode({
+                                    item = e.item,
+                                  amount = e.amount,
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
     end
 
     function object:onBonusByTalkingDataSDK(e)
         log.debug("onBonus: %s", e)
+
+        local ok, ret = luaj.callStaticMethod(TalkingDataSDK, "onBonus", { json.encode({
+                                     cmd = e.cmd,
+                                    coin = e.coin,
+                                  reason = e.reason,
+                        }) }, "(Ljava/lang/String;)V")
+        return ret
     end
 
     return component
